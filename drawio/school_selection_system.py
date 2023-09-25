16c81a262f091c4b5ad757653039d88c9f11856d
Ewooral <boahen.cos@gmail.com>
2023-09-25 14:14:19 +0000

    feat: Create a registrationAndLogin app in django

--- drawio/school_selection_system.py
+++ drawio/school_selection_system.py
@@ -2,53 +2,53 @@ class SchoolSystem:
     def __init__(self):
         self.universities = []
 
-    def addUniversity(self, university):
+    def add_university(self, university):
         self.universities.append(university)
 
-    def removeUniversity(self, university):
+    def remove_university(self, university):
         self.universities.remove(university)
 
-    def allocateSlot(self, student):
-        for university in self.universities:
-            if university.isSlotAvailable() and university.isEligible(student):
-                university.addStudent(student)
+    def allocate_slot(self, student):
+        for uni in self.universities:
+            if uni.is_slot_available() and uni.is_eligible(student):
+                uni.add_student(student)
                 break
 
 
 class University:
-    def __init__(self, name, grade, capacity, cutoffScore):
+    def __init__(self, name, grade, capacity, cutoff_score):
         self.name = name
         self.grade = grade
         self.capacity = capacity
-        self.slotsFilled = 0
+        self.slots_filled = 0
         self.students = []
-        self.cutoffScore = cutoffScore
+        self.cutoff_score = cutoff_score
 
-    def getName(self):
+    def get_name(self):
         return self.name
 
-    def getGrade(self):
+    def get_grade(self):
         return self.grade
 
-    def getCapacity(self):
+    def get_capacity(self):
         return self.capacity
 
-    def getSlotsFilled(self):
-        return self.slotsFilled
+    def get_slots_filled(self):
+        return self.slots_filled
 
-    def addStudent(self, student):
+    def add_student(self, student):
         self.students.append(student)
-        self.slotsFilled += 1
+        self.slots_filled += 1
 
-    def removeStudent(self, student):
+    def remove_student(self, student):
         self.students.remove(student)
-        self.slotsFilled -= 1
+        self.slots_filled -= 1
 
-    def isSlotAvailable(self):
-        return self.slotsFilled < self.capacity
+    def is_slot_available(self):
+        return self.slots_filled < self.capacity
 
-    def isEligible(self, student):
-        return student.getGrade() >= self.cutoffScore
+    def is_eligible(self, student):
+        return student.get_grade() >= self.cutoff_score
 
 
 class Student:
@@ -57,13 +57,13 @@ class Student:
         self.program = program
         self.grade = grade
 
-    def getName(self):
+    def get_name(self):
         return self.name
 
-    def getProgram(self):
+    def get_program(self):
         return self.program
 
-    def getGrade(self):
+    def get_grade(self):
         return self.grade
 
 
@@ -71,13 +71,13 @@ class Student:
 system = SchoolSystem()
 
 # Create universities
-university1 = University("University A", 'A', 6, 80)
-university2 = University("University B", 'B', 3, 75)
-university3 = University("University C", 'C', 1, 70)
+university1 = University("University of Ghana", 'A', 6, 80)
+university2 = University("University of Winneba", 'B', 3, 75)
+university3 = University("Methodist University", 'C', 1, 70)
 
-system.addUniversity(university1)
-system.addUniversity(university2)
-system.addUniversity(university3)
+system.add_university(university1)
+system.add_university(university2)
+system.add_university(university3)
 
 # Create students
 student1 = Student("John", "Computer Science", 85)
@@ -92,25 +92,28 @@ student9 = Student("James", "Economics", 79)
 student10 = Student("Emma", "Sociology", 82)
 
 # Allocate slots to students
-system.allocateSlot(student1)
-system.allocateSlot(student2)
-system.allocateSlot(student3)
-system.allocateSlot(student4)
-system.allocateSlot(student5)
-system.allocateSlot(student6)
-system.allocateSlot(student7)
-system.allocateSlot(student8)
-system.allocateSlot(student9)
-system.allocateSlot(student10)
+system.allocate_slot(student1)
+system.allocate_slot(student2)
+system.allocate_slot(student3)
+system.allocate_slot(student4)
+system.allocate_slot(student5)
+system.allocate_slot(student6)
+system.allocate_slot(student7)
+system.allocate_slot(student8)
+system.allocate_slot(student9)
+system.allocate_slot(student10)
 
 # Print allocated slots for each university
-for university in system.universities:
-    print(f"University: {university.getName()}")
-    print(f"Grade: {university.getGrade()}")
-    print(f"Capacity: {university.getCapacity()}")
-    print(f"Slots Filled: {university.getSlotsFilled()}")
+for uni in system.universities:
+    print(f"University: {uni.get_name()}")
+    print(f"Grade: {uni.get_grade()}")
+    print(f"Capacity: {uni.get_capacity()}")
+    print(f"Slots Filled: {uni.get_slots_filled()}")
     print("Students:")
-    for student in university.students:
+    for student in uni.students:
         print(
-            f"- Name: {student.getName()} | Program: ({student.getProgram()}) | Score: ({student.getGrade()})")
+            f"- Name: {student.get_name()} | Program: ({student.get_program()}) | Score: ({student.get_grade()})")
     print("\n")
+
+
+
