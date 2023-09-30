a4cb5ac859ddcf487ac2ca6c791fe90e20027f82
Ewooral <boahen.cos@gmail.com>
2023-09-30 00:06:00 +0000

    feat: Create an API to connect frontend to backend

---
+++ frontend/automated-sss/pages/about.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import { useState, useEffect, useReducer } from 'react'
+
+// https://api.github.com/users/ewooral
+
+export default function About() {
+    return (
+        <>
+            <h1>About Page</h1>
+            <div>
+              
+            </div>
+            <Link className='p-5' href="about/">Homepage</Link>
+        </>
+    )
+}
+
---
+++ frontend/automated-sss/pages/pages.tsx
@@ -0,0 +1,16 @@
+
+import Image from "next/image";
+import Navbar from "../src/app/navbar";
+import RegistrationForm from "../src/app/registrationForm";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <RegistrationForm />
+    </>
+    
+  );
+};
+
+export default Home;
